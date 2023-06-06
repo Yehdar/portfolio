3affa687eb49d99627e428716852e63f97dc6926
Yehdar <p.radhey06@gmail.com>
2023-06-06 11:09:43 -0400

    added more categories and more tailwindcss

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,5 +1,12 @@
 import React, { useState } from "react";
-import { AiOutlineMenu, AiOutlineHome } from "react-icons/ai";
+import {
+  AiOutlineMenu,
+  AiOutlineHome,
+  AiOutlineProject,
+  AiOutlineMail,
+} from "react-icons/ai";
+import { BsPerson } from "react-icons/bs";
+import { GrProjects } from "react-icons/gr";
 
 const Sidenav = () => {
   const [nav, setNav] = useState(false);
@@ -14,9 +21,40 @@ const Sidenav = () => {
       />
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
-          <a href="#main" className="w-[75%] flex justify-center">
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
+          >
             <AiOutlineHome size={20} />
             <span className="pl-4">Home</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
+          >
+            <GrProjects size={20} />
+            <span className="pl-4">Experience</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
+          >
+            <AiOutlineProject size={20} />
+            <span className="pl-4">Projects</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
+          >
+            <BsPerson size={20} />
+            <span className="pl-4">Resume</span>
+          </a>
+          <a
+            href="#main"
+            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
+          >
+            <AiOutlineMail size={20} />
+            <span className="pl-4">Contact</span>
           </a>
         </div>
       ) : (
