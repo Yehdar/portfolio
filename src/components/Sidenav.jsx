d4eb3c6ffb2c12b1082c53f1d0b5d22197adb8c4
Yehdar <p.radhey06@gmail.com>
2023-06-06 10:31:52 -0400

    relocated sidenav and added event handler

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {AiOutlineMenu} from 'react-icons/ai'
 
 const Sidenav = () => {
+    const[nav, setNav] = useState(false)
+    const handleNav = () => {
+      setNav(!nav);
+    };
   return (
     <div>
-        <AiOutlineMenu />
+        <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
+
     </div>
-  )
-}
+  );
+};
 
 export default Sidenav
