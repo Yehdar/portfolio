3f44b8e3c073a12daf0255f3e4850f8730d04139
Yehdar <p.radhey06@gmail.com>
2023-06-06 10:58:38 -0400

    added prettier formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { useState } from 'react'
-import Sidenav from './components/Sidenav.jsx'
+import { useState } from "react";
+import Sidenav from "./components/Sidenav.jsx";
 
 function App() {
-
   return (
     <div>
       <Sidenav />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
