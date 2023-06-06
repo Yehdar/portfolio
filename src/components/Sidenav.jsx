7165261d9e3d6366d6855fbcd7a7bd7cdb939bb1
Yehdar <p.radhey06@gmail.com>
2023-06-06 10:59:27 -0400

    added tailwind css to navbar and added prettier formatting

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,17 +1,29 @@
-import React, {useState} from 'react'
-import {AiOutlineMenu} from 'react-icons/ai'
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineHome } from "react-icons/ai";
 
 const Sidenav = () => {
-    const[nav, setNav] = useState(false)
-    const handleNav = () => {
-      setNav(!nav);
-    };
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+  };
   return (
     <div>
-        <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
-
+      <AiOutlineMenu
+        onClick={handleNav}
+        className="absolute top-4 right-4 z-[99] md:hidden"
+      />
+      {nav ? (
+        <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
+          <a href="#main" className="w-[75%] flex justify-center">
+            <AiOutlineHome size={20} />
+            <span className="pl-4">Home</span>
+          </a>
+        </div>
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
 
-export default Sidenav
+export default Sidenav;
