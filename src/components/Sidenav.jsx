f967454fa5db864bfbaf0295d1b818dbb7863e8e
Yehdar <p.radhey06@gmail.com>
2023-06-06 11:27:51 -0400

    updated sidenav to be desktop and mobile friendly

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -58,8 +58,18 @@ const Sidenav = () => {
           </a>
         </div>
       ) : (
-        <div></div>
+        ""
       )}
+      <div className="md:block hidden fixed top-[25%] z-10">
+        <div className="flex flex-col">
+          <a
+            href="#main"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <AiOutlineHome />
+          </a>
+        </div>
+      </div>
     </div>
   );
 };
