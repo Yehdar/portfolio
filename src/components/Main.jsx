2b93eb6338bf1b3f259eef7a89aff172498622cf
Yehdar <p.radhey06@gmail.com>
2023-06-07 11:47:01 -0400

    added tailwindcss

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,10 +11,12 @@ const Main = () => {
         alt="background image"
       />
       <div className="w-full h-screen absolute top-0 left-0 bg-white/50">
-        <div>
-          <h1>I'm Radhey Patel</h1>
-          <h2>
-            I'm
+        <div className="max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center">
+          <h1 className="sm:text-5xl text-4xl font-bold text-gray-800">
+            I am Radhey Patel
+          </h1>
+          <h2 className="flex sm:text-3xl text-2xl pt-4 text-gray-800">
+            {/* I'm */}
             <TypeAnimation
               sequence={[
                 "an up-and-coming developer", // Types 'One'
@@ -30,7 +32,7 @@ const Main = () => {
               style={{ fontSize: "1em", paddingLeft: "5px" }}
             />
           </h2>
-          <div>
+          <div className="flex justify-between pt-6 max-w-[200px] w-full">
             <FaLinkedinIn className="cursor-pointer" size={20} />
             <FaGithub className="cursor-pointer" size={20} />
             <FaTelegramPlane className="cursor-pointer" size={20} />
