37b02a9f301ae663b26e06c2398e969dc7f44233
Yehdar <p.radhey06@gmail.com>
2023-06-07 11:03:57 -0400

    added Main.jsx and background image

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Main = () => {
+  return (
+    <div id="main">
+      <img
+        className="w-full h-screen object-cover"
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwindows10spotlight.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fa57a663598e56ae7b5467be228112b82.jpg&f=1&nofb=1&ipt=bf0ce82dc526d5950b6f8b0e6939eecae1d2c9356c26edc25610b26a5133cace&ipo=images"
+        alt="background image"
+      />
+    </div>
+  );
+};
+
+export default Main;
