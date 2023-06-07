f4228cba3b96883e7917a491cf4d0f20bef5dc14
Yehdar <p.radhey06@gmail.com>
2023-06-07 11:06:02 -0400

    made background transparent

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -8,6 +8,7 @@ const Main = () => {
         src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwindows10spotlight.com%2Fwp-content%2Fuploads%2F2020%2F08%2Fa57a663598e56ae7b5467be228112b82.jpg&f=1&nofb=1&ipt=bf0ce82dc526d5950b6f8b0e6939eecae1d2c9356c26edc25610b26a5133cace&ipo=images"
         alt="background image"
       />
+      <div className="w-full h-screen absolute top-0 left-0 bg-white/50"></div>
     </div>
   );
 };
