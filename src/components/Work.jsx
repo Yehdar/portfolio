8cbc971bd1598d0f9b49895ae467b5d87aec358d
Yehdar <p.radhey06@gmail.com>
2023-06-08 23:02:32 -0400

    added work component and sample data

---
+++ src/components/Work.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import WorkItem from "./WorkItem";
+
+const data = [
+  {
+    year: 2016,
+    title: "Astronaut",
+    duration: "3 Years",
+    details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nulla, neque quidem rerum fuga tempora tenetur dicta minus tempore, expedita eaque, ipsum est rem optio qui velit dolorem inventore necessitatibus!",
+  },
+  {
+    year: 2014,
+    title: "Doctor",
+    duration: "2 Years",
+    details:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni nulla, neque quidem rerum fuga tempora tenetur dicta minus tempore, expedita eaque, ipsum est rem optio qui velit dolorem inventore necessitatibus!",
+  },
+];
+const Work = () => {
+  return <div>Work</div>;
+};
+
+export default Work;
