de924a1c393839a43bdcd45d44b538e56abb484b
Yehdar <p.radhey06@gmail.com>
2023-06-08 23:15:02 -0400

    imported Work container

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import Sidenav from "./components/Sidenav.jsx";
 import Main from "./components/Main.jsx";
+import Work from "./components/Work.jsx";
 
 function App() {
   return (
     <div>
       <Sidenav />
       <Main />
+      <Work />
     </div>
   );
 }
