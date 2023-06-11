eadc7688e4f32609cf1229550e10db476b7711ef
Yehdar <p.radhey06@gmail.com>
2023-06-10 22:55:09 -0400

    removed sections no longer needed in portfolio

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -25,61 +25,29 @@ const Sidenav = () => {
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
           >
-            <AiOutlineHome size={20} />
+            <BsPerson size={20} />
             <span className="pl-4">Home</span>
           </a>
           <a
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
           >
-            <GrProjects size={20} />
+            <AiOutlineProject size={20} />
             <span className="pl-4">Experience</span>
           </a>
           <a
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
           >
-            <AiOutlineProject size={20} />
+            <GrProjects size={20} />
             <span className="pl-4">Projects</span>
-          </a>
-          <a
-            href="#main"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
-          >
-            <BsPerson size={20} />
-            <span className="pl-4">Resume</span>
-          </a>
-          <a
-            href="#contact"
-            className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
-          >
-            <AiOutlineMail size={20} />
-            <span className="pl-4">Contact</span>
           </a>
         </div>
       ) : (
         ""
       )}
-      <div className="md:block hidden fixed top-[25%] z-10">
+      <div className="md:block  hidden fixed top-[40%] z-10">
         <div className="flex flex-col">
-          <a
-            href="#main"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
-          >
-            <AiOutlineHome size={20} />
-          </a>
-          <a
-            href="#work"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
-          >
-            <GrProjects size={20} />
-          </a>
-          <a
-            href="#projects"
-            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
-          >
-            <AiOutlineProject size={20} />
-          </a>
           <a
             href="#main"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
@@ -87,10 +55,16 @@ const Sidenav = () => {
             <BsPerson size={20} />
           </a>
           <a
-            href="#contact"
+            href="#work"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
-            <AiOutlineMail size={20} />
+            <AiOutlineProject size={20} />
+          </a>
+          <a
+            href="#projects"
+            className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
+          >
+            <GrProjects size={20} />
           </a>
         </div>
       </div>
