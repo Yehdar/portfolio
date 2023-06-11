ad33e5219942a1d3f66384125cc5602c19f65593
Yehdar <p.radhey06@gmail.com>
2023-06-10 23:03:34 -0400

    fixed navbar on mobile by re-using the handleNav function

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -22,6 +22,7 @@ const Sidenav = () => {
       {nav ? (
         <div className="fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20">
           <a
+            onClick={handleNav}
             href="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
           >
@@ -29,6 +30,7 @@ const Sidenav = () => {
             <span className="pl-4">Home</span>
           </a>
           <a
+            onClick={handleNav}
             href="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
           >
@@ -36,6 +38,7 @@ const Sidenav = () => {
             <span className="pl-4">Experience</span>
           </a>
           <a
+            onClick={handleNav}
             href="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-opinter hover:scale-110 ease-in dureation-200"
           >
