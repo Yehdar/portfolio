5a7a9b7d238c7b24a55fb46bbedff0c54e95a052
Yehdar <p.radhey06@gmail.com>
2023-06-12 19:19:25 -0400

    modified text color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="m-auto md:pl-20 p-4 py-5 text-center">
+    <div className="m-auto md:pl-20 p-4 py-5 text-center text-white">
       <p className="text-sm mt-2 opacity-50">
         &copy; {new Date().getFullYear()} Radhey Patel. All rights Reserved.
       </p>
