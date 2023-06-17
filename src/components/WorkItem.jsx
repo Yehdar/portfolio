c4e4d1d81e9d8b95616bb6c0b316bf2aeb1ad8d5
Yehdar <p.radhey06@gmail.com>
2023-06-16 23:13:15 -0400

    reformatted to look better

--- src/components/WorkItem.jsx
+++ src/components/WorkItem.jsx
@@ -3,15 +3,19 @@ import React from "react";
 const WorkItem = ({ year, title, duration, details }) => {
   return (
     <ol className="flex flex-col md:flex-row relative border-l border-stone-200">
-      <li className="mb-10 ml-4">
+      <li className="mb-10 ml-5">
         <div className="absolute w-3 h-3 bg-stone-200 rounded-full mt-1.5 -left-1.5 border-white" />
-        <p className="flex flex-wrap gap-4 flex-row items-center justify-start text-xs md:text-sm">
-          <span className="inline-block px-2 py-1 font-semibold text-white text-lg bg-yellow-600 rounded-md">
-            {year}
-          </span>
+        <p>
           <span className="text-3xl font-semibold text-yellow-400">
             {title}
           </span>
+          <p className="text-right -mt-9">
+            <span className="mx-20 inline-block px-2 py-1 font-semibold text-right text-white text-lg bg-yellow-600 rounded-md">
+              {year}
+            </span>
+          </p>
+        </p>
+        <p>
           <span className="my-1 text-lg font-normal leading-none text-gray-300">
             {duration}
           </span>
