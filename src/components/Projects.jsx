fd57e9d3d5901fce9b9649be2c112570c88ea9c3
Yehdar <p.radhey06@gmail.com>
2023-07-04 12:35:37 -0400

    refactor: add boilerplate for more projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,71 @@
 import React from "react";
 import ProjectItem from "./ProjectItem";
-import TCP from "../assets/tcp_messager_working.png";
+import TCP from "../assets/projectbg/tcp_messager_working.png";
 
 const Projects = () => {
   return (
-    <div id="projects" className="max-w-[1040px] m-auto md:pl-20 p-4 py-16">
+    <div id="projects" className="max-w-[1540px] m-auto md:pl-20 p-4 py-16">
       <h1 className="text-4xl font-bold text-center text-yellow-400">
         Projects
       </h1>
       <p className="text-center py-8 text-white text-2xl">
-        Currently, I am learning Golang and Rust by rewritting famous software.
+        Currently, I am learning Rust and Go by rewritting famous software.
       </p>
       <div className="grid sm:grid-cols-2 gap-12">
+        <ProjectItem
+          bg={TCP}
+          name="LAN Chat"
+          description="local network chatting app in Python"
+          hyperlink="https://github.com/Yehdar/LAN-party-chat"
+        />
+        <ProjectItem
+          bg={TCP}
+          name="LAN Chat"
+          description="local network chatting app in Python"
+          hyperlink="https://github.com/Yehdar/LAN-party-chat"
+        />
+        <ProjectItem
+          bg={TCP}
+          name="LAN Chat"
+          description="local network chatting app in Python"
+          hyperlink="https://github.com/Yehdar/LAN-party-chat"
+        />
+        <ProjectItem
+          bg={TCP}
+          name="LAN Chat"
+          description="local network chatting app in Python"
+          hyperlink="https://github.com/Yehdar/LAN-party-chat"
+        />
+        <ProjectItem
+          bg={TCP}
+          name="LAN Chat"
+          description="local network chatting app in Python"
+          hyperlink="https://github.com/Yehdar/LAN-party-chat"
+        />
+        <ProjectItem
+          bg={TCP}
+          name="LAN Chat"
+          description="local network chatting app in Python"
+          hyperlink="https://github.com/Yehdar/LAN-party-chat"
+        />
+        <ProjectItem
+          bg={TCP}
+          name="LAN Chat"
+          description="local network chatting app in Python"
+          hyperlink="https://github.com/Yehdar/LAN-party-chat"
+        />
+        <ProjectItem
+          bg={TCP}
+          name="LAN Chat"
+          description="local network chatting app in Python"
+          hyperlink="https://github.com/Yehdar/LAN-party-chat"
+        />
+        <ProjectItem
+          bg={TCP}
+          name="LAN Chat"
+          description="local network chatting app in Python"
+          hyperlink="https://github.com/Yehdar/LAN-party-chat"
+        />
         <ProjectItem
           bg={TCP}
           name="LAN Chat"
