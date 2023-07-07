c1885e99a96e153e482e7b8bb5c30e432eba9942
Yehdar <p.radhey06@gmail.com>
2023-07-07 08:18:22 -0400

    revert changes

--- vite.config.js
+++ vite.config.js
@@ -5,26 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/portfolio",
-
-  server: {
-    // other server options...
-
-    // Add MIME type configuration for JavaScript modules
-    // This code should be placed under the `server` configuration
-    // if the server is being used during development
-    hmr: {
-      // other hmr options...
-      // ...
-
-      // Add the MIME type configuration inside `hmr` object
-      onBeforeReconnect({ server }) {
-        server.middlewareManager.middlewares.use((req, res, next) => {
-          if (req.originalUrl === '/index.jsx') {
-            res.type('text/jsx');
-          }
-          next();
-        });
-      },
-    },
-  },
 });
